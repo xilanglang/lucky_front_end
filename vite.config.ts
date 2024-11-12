548d456663bd478d4188a2d1e19c9905c3c363b9
辛少丰 <xinshaof@yonyou.com>
2024-11-12 12:30:59 +0800

    添加工作流

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,8 @@ import react from '@vitejs/plugin-react';
 import { nodePolyfills } from 'vite-plugin-node-polyfills';
 
 // https://vitejs.dev/config/
+// 这是 GitHub 的规则：仓库名称必须是基本路径。所以我们就把/lucky_front_end/作为项目的基础：
 export default defineConfig({
   plugins: [react(), nodePolyfills()],
-  base: '/',
+  base: '/lucky_front_end/',
 });
